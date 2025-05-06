ccdeb0e3b3d19c5516972ef99c715d73efec9526
Mathias Bynens <mathias@qiwi.be>
2025-05-06 08:23:41 +0200

    Release v1.6.6

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export const Binary_Property: string[];
+export const General_Category: string[];
+export const Bidi_Class: string[];
+export const Block: string[];
+export const Names: string[];
+export const Simple_Case_Mapping: string[];
+export const Special_Casing: string[];
